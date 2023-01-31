d9b18a5ab09c212569bc31fd1e5d3aa0d0774a52
Matt0107 <mathieu.durimel@gmail.com>
2023-01-31 23:25:03 +0100

    start

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>WELCOME</h1>
+      <img src="../images/Afrotennis_white.png" alt="" />
+    </div>
+  );
+};
+
+export default Home;
