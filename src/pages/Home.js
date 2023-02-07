5ce6321519895fd76439f3203475213a451aee24
Matt0107 <mathieu.durimel@gmail.com>
2023-02-07 12:53:50 +0100

    small change on pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,23 @@ import "../styles/Home.css";
 const Home = () => {
   return (
     <div>
-      <h1>test</h1>
+      <div className="text">
+        <h1>Celebrate African Tennis</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          perferendis voluptatibus quam qui accusantium inventore dolorem
+          exercitationem ex in dignissimos minima eveniet quidem cupiditate,
+          fuga aliquam, autem ut at magni, nam ipsam provident quos tempore.
+          Tempore doloremque corporis impedit id iste, aut suscipit saepe
+          perferendis voluptatem, consequatur reiciendis error fugiat dolorem?
+          Porro quisquam doloremque fugit ad eaque corporis nihil, enim illo
+          aperiam nam. Voluptates, officiis quis! Facilis illo odit officia
+          labore adipisci illum, perferendis mollitia autem ab repudiandae at
+          dicta expedita doloribus? Vel tempore illum alias doloribus eaque
+          necessitatibus? Reiciendis quis accusantium temporibus delectus ab!
+          Officia nostrum fuga esse odit.
+        </p>
+      </div>
     </div>
   );
 };
