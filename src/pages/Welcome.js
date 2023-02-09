3c7e047ddf59b76a512aa51e68be44c063870de1
Matt0107 <mathieu.durimel@gmail.com>
2023-02-09 11:40:20 +0100

    corrections en cours

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,7 +4,7 @@ import image from "../assets/Afrotennis_white.png";
 
 const Welcome = () => {
   return (
-    <div>
+    <div className="welcome-page">
       <img src={image} alt="logo" className="logo-welcome" />
       <a href="/Home">
         <button className="btn-welcome">ENTER</button>
